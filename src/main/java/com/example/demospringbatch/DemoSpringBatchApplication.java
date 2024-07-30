c26361493709204ea07c49bcac30a6ff1ca2cf76
mariusz woda <mariusz.woda@gmail.com>
2024-07-30 13:00:27 +0200

    method for sorting generated by ai assistant

--- src/main/java/com/example/demospringbatch/DemoSpringBatchApplication.java
+++ src/main/java/com/example/demospringbatch/DemoSpringBatchApplication.java
@@ -10,4 +10,19 @@ public class DemoSpringBatchApplication {
         SpringApplication.run(DemoSpringBatchApplication.class, args);
     }
 
+    //  method to sort the input value
+
+    public void sortInputParams(int[] inputParams) {
+        int n = inputParams.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (inputParams[j] > inputParams[j + 1]) {
+                    // swap inputParams[j+1] and inputParams[i]
+                    int temp = inputParams[j];
+                    inputParams[j] = inputParams[j + 1];
+                    inputParams[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
